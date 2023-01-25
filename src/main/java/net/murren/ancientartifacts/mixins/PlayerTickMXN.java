5c74d8f5c880c41dc628610af7b1db932c600037
Murren <merwou@hotmail.com>
2023-01-25 21:26:54 +0100

    lol both toughness and hunters artifact not working sob

--- src/main/java/net/murren/ancientartifacts/mixins/PlayerTickMXN.java
+++ src/main/java/net/murren/ancientartifacts/mixins/PlayerTickMXN.java
@@ -15,59 +15,47 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static net.murren.ancientartifacts.item.ArtifactItems.*;
 
 @Mixin(PlayerEntity.class)
-abstract class PlayerTickMXN
-{
+abstract class PlayerTickMXN {
     @Shadow
     public PlayerInventory getInventory() {
         return null;
     }
 
-    @Shadow public abstract HungerManager getHungerManager();
+    @Shadow
+    public abstract HungerManager getHungerManager();
 
     @Inject(method = "tick", at = @At("HEAD"))
-    private void onAttributes(CallbackInfo ci)
-    {
+    private void onAttributes(CallbackInfo ci) {
         PlayerInventory inv = getInventory();
 
         boolean taFound = false;
-        for(int i = 0; i < inv.size(); i++)
-        {
-            if(inv.getStack(i).getItem().equals(toughness_artifact))
-            {
+        for (int i = 0; i < inv.size(); i++) {
+            if (inv.getStack(i).getItem().equals(toughness_artifact)) {
                 taFound = true;
             }
-            if(inv.getStack(i).getItem().equals(saturation_artifact))
-            {
-                if (getHungerManager().isNotFull())
-                {
+            if (inv.getStack(i).getItem().equals(saturation_artifact)) {
+                if (getHungerManager().isNotFull()) {
                     getHungerManager().setFoodLevel(20);
                 }
             }
         }
-        if(taFound)
-        {
+        if (taFound) {
             PlayerEntity.createPlayerAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 40f);
-        }
-        else
-        {
+        } else {
             PlayerEntity.createPlayerAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 20f);
         }
 
         boolean haFound = false;
 
-        if(inv.getMainHandStack().getItem() instanceof BowItem || inv.getMainHandStack().getItem() instanceof CrossbowItem);
+        if (inv.getMainHandStack().getItem() instanceof BowItem || inv.getMainHandStack().getItem() instanceof CrossbowItem);
         {
-            for (int i = 0; i < inv.size(); i++)
-            {
-                if (inv.getStack(i).getItem().equals(hunter_artifact))
-                {
+            for (int i = 0; i < inv.size(); i++) {
+                if (inv.getStack(i).getItem().equals(hunter_artifact)) {
                     haFound = true;
                 }
             }
-            if (haFound)
-            {
-                if(inv.getMainHandStack().getItem() instanceof BowItem)
-                {
+            if (inv.getMainHandStack().getItem() instanceof BowItem) {
+                if (haFound) {
                     inv.getMainHandStack().getOrCreateNbt().putBoolean("artifact", true);
                 }
                 else {
@@ -77,3 +65,4 @@ abstract class PlayerTickMXN
         }
     }
 }
+
