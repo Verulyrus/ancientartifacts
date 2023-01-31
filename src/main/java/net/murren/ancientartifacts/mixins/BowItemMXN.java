459bc78792e59a83ebddb74ac00e9a33fd355bcd
Verulyrus <merwou@hotmail.com>
2023-01-31 12:34:46 +0100

    repaired bowitem mixin

--- src/main/java/net/murren/ancientartifacts/mixins/BowItemMXN.java
+++ src/main/java/net/murren/ancientartifacts/mixins/BowItemMXN.java
@@ -1,28 +1,44 @@
 package net.murren.ancientartifacts.mixins;
 
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BowItem.class)
 public abstract class BowItemMXN {
 
 
-    @Inject(method = "releaseUsing", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/BowItem;getPowerForTime(I)F"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    public void doublef(ItemStack itemStack, Level level, LivingEntity livingEntity, int i, CallbackInfo ci, float f)
+    @Inject(method = "getUseDuration", at = @At("HEAD"))
+    public void useDur(ItemStack itemStack, CallbackInfoReturnable<Integer> cir)
     {
-        assert itemStack.getTag() != null;
-        if(itemStack.getTag().contains("artifact"))
+        if(itemStack.getOrCreateTag().contains("artifact"))
         {
-            if(itemStack.getTag().getBoolean("artifact")) {
-                f = 2 * f;
+            if(itemStack.getTag().getBoolean("artifact"))
+            {
+                cir.setReturnValue(144000);
             }
+            else cir.setReturnValue(60000);
         }
     }
+
+    @Inject(method = "getPowerForTime", at = @At("TAIL"))
+    private static void getPow(int i, CallbackInfoReturnable<Float> cir)
+    {
+        if(i > 70000)
+        {
+            float k = (float)i / 10.0F;
+            k = (k * k + k * 2.0F) / 3.0F;
+            if (k > 1.0F) {
+                k = 1.0F;
+            }
+
+            cir.setReturnValue(k);
+        }
+    }
+
+
+
 }
