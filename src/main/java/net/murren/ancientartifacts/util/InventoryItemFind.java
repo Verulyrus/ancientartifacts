d086f8de6fedc72642940fe3254a61211dce3f49
Murren <merwou@hotmail.com>
2023-02-08 20:34:20 +0100

    shulker support stuff

--- src/main/java/net/murren/ancientartifacts/util/InventoryItemFind.java
+++ src/main/java/net/murren/ancientartifacts/util/InventoryItemFind.java
@@ -1,8 +1,13 @@
 package net.murren.ancientartifacts.util;
 
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
+import static net.minecraft.world.item.Items.SHULKER_BOX;
+import static net.murren.ancientartifacts.util.sendLog.logMessage;
 import static net.murren.ancientartifacts.util.tagKeys.Items.SHULKER_BOXES;
 
 public class InventoryItemFind {
@@ -12,9 +17,16 @@ public class InventoryItemFind {
             if (inv.getItem(i).getItem().equals(item)) {
                 return true;
             }
-            if(inv.getItem(i).is(SHULKER_BOXES))
+            if(inv.getItem(i).is(SHULKER_BOX) && inv.getItem(i).hasTag())
             {
-
+                logMessage("found shulker box with tag");
+                NonNullList<ItemStack> box = NonNullList.create();
+                ContainerHelper.loadAllItems(inv.getItem(i).getTag(), box);
+                if(box.stream().anyMatch(stack -> stack.is(item)))
+                {
+                    logMessage("found shulker box containing item");
+                    return true;
+                }
             }
         }
         return false;
